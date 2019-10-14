82815a2d71b627acfb3aade23c924dd40b6bb0f9
Rafal Kaniewski <rafal.kaniewski@gmail.com>
2019-10-14 14:38:59 +0200

    Done challenge3 in objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -68,10 +68,16 @@ console.log(internGannie.name);
 console.log(internAntonietta.gender);
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+internKennan.speak = function() {
+  return `Hello, my name is ${this.name}`;
+};
+console.log(internKennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+internAntonietta.multiplyNums = function(num1, num2) {
+  return num1 * num2;
+};
+console.log(internAntonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
